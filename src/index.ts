59a3c7a15caa33f1e15513483eafab344969ff87
HM <hieumdd@gmail.com>
2022-06-24 14:21:41 +0700

    feat: Add GoogleKeyword to app

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { http } from '@google-cloud/functions-framework';
 import express from 'express';
 
 import ConversionController from './feature/conversion/conversion.controller';
+import GoogleController from './feature/google/google.controller';
 
 const app = express();
 
-app.get('/conversion', ConversionController);
+app.use('/conversion', ConversionController);
+app.get('/google/keyword', GoogleController);
 
 http('main', app);
