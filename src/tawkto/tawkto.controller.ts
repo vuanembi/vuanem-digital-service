5e43d264d48b19257f9266fc73fac0a9a96665b8
HM <hieumdd@gmail.com>
2022-09-07 14:53:51 +0700

    feat: Add error catch

--- src/tawkto/tawkto.controller.ts
+++ src/tawkto/tawkto.controller.ts
@@ -7,13 +7,13 @@ import { webhook } from './webhook.service';
 export const tawktoController = express.Router();
 
 tawktoController.post('/prechat-submit', (req: Request, res: Response) => {
-    webhookService(req.body, prechatSubmit).then(() =>
-        res.status(200).json({ result: 'ok' }),
-    );
+    webhookService(req.body, prechatSubmit)
+        .then(() => res.status(200).json({ result: 'ok' }))
+        .catch((error) => res.status(500).json({ error }));
 });
 
 tawktoController.post('/webhook', (req: Request, res: Response) => {
-    webhookService(req.body, webhook).then(() =>
-        res.status(200).json({ result: 'ok' }),
-    );
+    webhookService(req.body, webhook)
+        .then(() => res.status(200).json({ result: 'ok' }))
+        .catch((error) => res.status(500).json({ error }));
 });
