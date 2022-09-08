46956b72d202034aa2429d7731f5fdddba763c52
HM <hieumdd@gmail.com>
2022-09-08 10:10:13 +0700

    feat: Add utm queries to PrechatSubmit

--- src/tawkto/track.service.ts
+++ src/tawkto/track.service.ts
@@ -8,17 +8,32 @@ import { WebhookOptions } from '../webhook/webhook.service';
 import { PrechatSubmitDto } from './track.dto';
 
 export const prechatSubmit: WebhookOptions<PrechatSubmitDto> = {
-    parseFn: ({ phone, name }) => [
-        { phone, name, id: uuidv4(), created_at: dayjs.utc().toISOString() },
+    parseFn: (data) => [
+        {
+            id: uuidv4(),
+            created_at: dayjs.utc().toISOString(),
+            phone: data.phone,
+            name: data.name,
+            utm_source: data.utm_source,
+            utm_medium: data.utm_medium,
+            utm_id: data.utm_id,
+            utm_campaign: data.utm_campaign,
+            utm_content: data.utm_content,
+        },
     ],
     options: {
         dataset: 'IP_TawkTo',
         table: 'PrechatSubmit',
         schema: [
             { name: 'id', type: 'STRING' },
+            { name: 'created_at', type: 'TIMESTAMP' },
             { name: 'phone', type: 'STRING' },
             { name: 'name', type: 'STRING' },
-            { name: 'created_at', type: 'TIMESTAMP' },
+            { name: 'utm_source', type: 'STRING' },
+            { name: 'utm_medium', type: 'STRING' },
+            { name: 'utm_id', type: 'STRING' },
+            { name: 'utm_campaign', type: 'STRING' },
+            { name: 'utm_content', type: 'STRING' },
         ],
     },
 };
