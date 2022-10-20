ae1ecddac4cfa26c99291704b690c8c2de3173e8
HM <hieumdd@gmail.com>
2022-10-20 17:45:38 +0700

    feat: Add purchase-acquisition

--- src/facebook/facebook.controller.ts
+++ src/facebook/facebook.controller.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
 
 import { conversionService } from '../conversion/conversion.service';
-import { conversion } from './facebook.service';
+import { conversion, conversionAcquisition } from './facebook.service';
 
 export const facebookController = express.Router();
 
 facebookController.get('/conversion', (req: Request, res: Response) => {
-    conversionService(conversion, req.query.date as string)
+    Promise.all(
+        [conversion, conversionAcquisition].map((service) =>
+            conversionService(service, req.query.date as string),
+        ),
+    )
         .then((num) => {
             console.log({ num });
             res.status(200).json({ num });
--- src/facebook/facebook.service.test.ts
+++ src/facebook/facebook.service.test.ts
@@ -1,9 +1,18 @@
-import { conversion } from './facebook.service';
+import { conversion, conversionAcquisition } from './facebook.service';
 
 it('Conversion', async () => {
     const date = '2022-09-24';
 
     return conversion(date).then((res) => {
+        console.log(res);
+        expect(res).toBeGreaterThan(0);
+    });
+});
+
+it('Conversion Acquisition', async () => {
+    const date = '2022-10-19';
+
+    return conversionAcquisition(date).then((res) => {
         console.log(res);
         expect(res).toBeGreaterThan(0);
     });
